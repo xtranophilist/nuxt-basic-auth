96585babadb7c4e7d024c8ddd0d5848608f5632e
Dipesh Acharya <xtranophilist@gmail.com>
2022-11-07 11:44:58 +0545

    fix: revert to using render:response nitro hook

--- src/runtime/nitro.ts
+++ src/runtime/nitro.ts
@@ -3,10 +3,11 @@ import { requireModule } from '@nuxt/kit'
 const auth = requireModule('basic-auth')
 
 export default <NitroAppPlugin> function (nitro) {
-  nitro.hooks.hook('render:html', (response: RenderResponse, { event }) => {
+  nitro.hooks.hook('render:response', (response: RenderResponse, { event }) => {
     const credentials = auth(event.req)
     if (!credentials || credentials.name !== 'user' || credentials.pass !== 'pass') {
       response.statusCode = 401
+      response.headers = response.headers || {}
       response.headers['WWW-Authenticate'] = 'Basic realm="example"'
       response.body = 'Access Denied!'
     }
